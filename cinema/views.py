2d5ea41782bc17c6297d5f5202945f7b8e81ce6f
git_practic <dogoberrr@gmail.com>
2025-02-08 13:19:59 +0200

    implemented two views with names movie_list_view & movie_detail_view; add rest_framework to installed apps inside app/settings

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET"])
+def movie_list_view(request: HttpRequest) -> Response | None:
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def movie_detail_view(request: HttpRequest, pk: int) -> Response | None:
+    if request.method == "GET":
+        movie = get_object_or_404(Movie, pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
